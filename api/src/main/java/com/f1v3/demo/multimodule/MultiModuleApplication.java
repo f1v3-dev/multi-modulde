873ea805b7bc41b252c96009a9db99f7ca476000
seungjo <f1v3@kakao.com>
2025-01-18 14:06:58 +0900

    first commit

---
+++ api/src/main/java/com/f1v3/demo/multimodule/MultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.f1v3.demo.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/f1v3/demo/multimodule/MultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.demo.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
